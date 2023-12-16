7304f5f84a0169040ed8b1b6aab80c33350d593b
Wadad Parker <wadadparker5@gmail.com>
2023-12-16 23:07:05 +0530

    feat: Add workspace logic

--- src/components/workspace/CreateWorkspace.jsx
+++ src/components/workspace/CreateWorkspace.jsx
@@ -1,10 +1,31 @@
 import Image from 'next/image';
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from "../../../public/Logo.svg";
 import ModalBg1 from "../../../public/ModalBg1.svg";
 import ModalBg2 from "../../../public/ModalBg2.svg";
 
 const CreateWorkspace = ({setShowModal}) => {
+    const [input,setInput] = useState({workspaceName:"",workspaceid:""});
+    const [flag, setFlag] = useState(false);
+
+    const changeHandler=(inputField,text)=>
+    {
+        setInput(prev=>({...prev,[inputField]:text}));
+    }
+
+    const clickHandler=()=>
+    {
+        if(flag)
+        {
+
+
+            // Enter the actual function here to call workspace api
+            console.log(input);
+            setShowModal(false);
+        }
+        setFlag(true);
+    }
+
   return (
     <div className='fixed bg-black/30 w-screen h-screen z-40 top-0 flex justify-center items-center'>
         <main className='relative flex flex-col items-center bg-white min-w-1/3 min-h-1/2 max-h-max max-w-max rounded-lg shadow-lg pt-7 pl-5 pr-7 pb-16'>
@@ -19,8 +40,16 @@ const CreateWorkspace = ({setShowModal}) => {
             </section>
             <h1 className='text-2xl font-semibold -mt-5'>Create a Workspace</h1>
             <p className='text-[#525252] text-[19px] mt-3 mb-6'>Start creating a workspace to manage your teams</p>
-            <input className='py-3 px-2.5 border border-[#DED2D9] rounded-md placeholder:text-xl w-[85%]' placeholder='Enter a workspace name'/>
-            <button className='bg-primary text-white text-xl py-4 mt-5 w-[85%] rounded-full hover:shadow-sm hover:shadow-primary'>Next</button>
+            <input className='py-3 px-2.5 border border-[#DED2D9] rounded-md placeholder:text-xl w-[85%]'
+             placeholder={flag?'Enter a Workspace ID':'Enter a workspace name'}
+             value={flag?input.workspaceid:input.workspaceName}
+             onChange={(e)=>flag?changeHandler("workspaceid",e.target.value):changeHandler("workspaceName",e.target.value)}
+            />
+            <button className='bg-primary text-white text-xl py-4 mt-5 w-[85%] rounded-full hover:shadow-sm hover:shadow-primary disabled:brightness-75 disabled:hover:cursor-not-allowed'
+             disabled={flag?input.workspaceid==="":input.workspaceName===""}
+             onClick={clickHandler}>
+                {flag?"Create Workspace":"Next"}
+            </button>
         </main>
     </div>
   )
