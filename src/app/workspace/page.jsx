13ca2df4ed198d3313be0f502303d3ea60b9e04b
LifeAsDev <angelocraft551@gmail.com>
2023-12-18 19:30:35 -0400

    fixed: workspace page to uppercase

--- src/app/workspace/page.jsx
+++ src/app/workspace/page.jsx
@@ -1,15 +1,20 @@
-'use client'
-import React, {useState} from 'react'
-import CreateWorkspace from '@/components/workspace/CreateWorkspace';
+"use client";
+import React, { useState } from "react";
+import CreateWorkspace from "@/components/workspace/CreateWorkspace";
 
-export default function page () 
-{
-    const [showModal,setShowModal] = useState(false);
-    return (
-        <>{showModal && <CreateWorkspace setShowModal={setShowModal} />}
-        <div className='flex justify-center items-center'>
-            <button className='p-3 bg-primary text-white absolute top-2/4 right-1/2 z-30' onClick={()=>setShowModal(true)}>Click to open modal</button>
-        </div>
-        </>
-    )
+export default function Page() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      {showModal && <CreateWorkspace setShowModal={setShowModal} />}
+      <div className="flex justify-center items-center">
+        <button
+          className="p-3 bg-primary text-white absolute top-2/4 right-1/2 z-30"
+          onClick={() => setShowModal(true)}
+        >
+          Click to open modal
+        </button>
+      </div>
+    </>
+  );
 }
