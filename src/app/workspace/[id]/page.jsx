eef99a5679a938d1b787dbde95981993e04e7835
Wadad Parker <wadadparker5@gmail.com>
2023-12-23 10:34:27 +0530

    style: Added search bar

--- src/app/workspace/[id]/page.jsx
+++ src/app/workspace/[id]/page.jsx
@@ -19,6 +19,12 @@ export default function Page({params}) {
             <nav className='flex justify-between p-2'>
                 <span></span>
                 {/* Enter searchbar here */}
+                <aside className='flex items-center w-96 bg-gradient-to-r from-[#8E629A] to-[#7D5584] rounded-md pl-2 space-x-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-3.5 h-3.5 text-white">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                    <input className='outline-none bg-transparent text-white placeholder:text-white text-xs placeholder:text-xs' placeholder='Search Hitch'/>
+                </aside>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                 </svg>
