6e7c87129f135b9129f7af8a8081cdbb5599df61
Wadad Parker <wadadparker5@gmail.com>
2023-12-27 13:19:44 +0530

    feat: Delete channel

--- src/app/test-Chatbox/page.jsx
+++ src/app/test-Chatbox/page.jsx
@@ -4,17 +4,21 @@ import Chatbox from '@/components/Chatbox/Chatbox'
 import AddChannelModal from '@/components/modals/AddChannelModal'
 import AddPeopleModal from '@/components/modals/AddPeopleModal'
 import AddSubChannelModal from '@/components/modals/AddSubChannelModal'
+import DeleteChannelModal from '@/components/modals/DeleteChannelModal'
 
 const Page = () => {
   const [showChannelModal,setShowChannelModal] = useState(false);
   const [showPeopleModal,setShowPeopleModal] = useState(false);
   const [showSubChannelModal,setShowSubChannelModal] = useState(false);
+  const [showDeleteChannelModal,setShowDeleteChannelModal] = useState(false);
   return (
     <>
    <Chatbox/>
    <button className='bg-primary text-white px-2 py-1 rounded-md mx-5' onClick={()=>setShowChannelModal(true)}>Click to open create channel modal</button>
    <button className='bg-primary text-white px-2 py-1 rounded-md mx-5' onClick={()=>setShowPeopleModal(true)}>Click to open add people modal</button>
    <button className='bg-primary text-white px-2 py-1 rounded-md mx-5' onClick={()=>setShowSubChannelModal(true)}>Click to open add sub channel modal</button>
+   <button className='bg-primary text-white px-2 py-1 rounded-md mx-5' onClick={()=>setShowDeleteChannelModal(true)}>Click to open Delete Channel modal</button>
+   {showDeleteChannelModal && <DeleteChannelModal channelName="Frontend" setShowModal={setShowDeleteChannelModal}/> }
    {showSubChannelModal && <AddSubChannelModal setShowModal={setShowSubChannelModal}/> }
    {showChannelModal && <AddChannelModal setShowModal={setShowChannelModal}/> }
    {showPeopleModal && <AddPeopleModal setShowModal={setShowPeopleModal} /> }
---
+++ src/components/modals/DeleteChannelModal.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const DeleteChannelModal = ({channelName,setShowModal}) => {
+
+    const clickHandler=()=>
+    {
+        // Enter actual function to call API here
+        setShowModal(false);
+    }
+  return (
+    <div className='fixed bg-black/30 w-screen h-screen z-40 top-0 flex justify-center items-center'>
+        <main className='flex flex-col items-start bg-white  min-h-fit max-h-max min-w-[25%] max-w-[45%] rounded-lg shadow-lg pt-3 pl-5 pr-7 pb-5 max-sm:h-screen max-sm:justify-center max-sm:min-w-full'>
+            <h1 className='text-2xl font-bold'>Delete Channel</h1>
+            <p className='max-w-md text-[#525252] mt-3'>Are you sure you want to delete <b>#{channelName}</b>? This cannot be undone.</p>
+            <section className='mt-3 flex justify-between items-center w-full'>
+                <span className='text-[#464446]/75 grow font-medium'></span>
+                <button className='py-1.5 px-5 rounded-lg'>Cancel</button>
+                <button className='bg-red-600 text-white font-light py-1.5 px-5 rounded-lg' onClick={clickHandler}>Delete Channel</button>
+            </section>
+        </main>
+        </div>
+  )
+}
+
+export default DeleteChannelModal
