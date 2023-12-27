514a4be39de113b56201c76d9b52de891a37e6ae
Wadad Parker <wadadparker5@gmail.com>
2023-12-27 06:32:23 +0530

    style: Add people modal

--- src/components/modals/AddChannelModal.jsx
+++ src/components/modals/AddChannelModal.jsx
@@ -9,10 +9,13 @@ const AddChannelModal = ({setShowModal}) => {
             </svg>
             <h1 className='text-3xl font-bold mb-3'>Create a Channel</h1>
             <label htmlFor='name' className='text-lg font-bold'>Name</label>
+            <aside className='relative w-full'>
             <input className='w-full py-3 px-2' placeholder='# Enter your channel name' />
+            <span className='absolute right-2 top-3 text-text'>25</span>
+            </aside>
             <small className='max-w-md text-[#525252] mt-1.5'>Channels are  converastion that we have on any topics,Use the name which is easy to find and understand </small>
             <section className='mt-3 flex justify-between items-center w-full'>
-                <span className='text-text grow'>Step 1 of 2</span>
+                <span className='text-[#464446]/75 grow font-medium'>Step 1 of 2</span>
                 <button className='py-1.5 px-5 rounded-lg border-2 border-black/10 shadow-lg font-semibold shadow-black/20 mr-2'>Back</button>
                 <button className='bg-[#1C0C40] border-2 text-white font-light py-1.5 px-5 rounded-lg'>Next</button>
             </section>
